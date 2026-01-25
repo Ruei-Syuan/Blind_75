9f4b0533b42c7c6dd02524467cc9ec6b8a5a7625
Ruei-Syuan <syuan111251@gmail.com>
2026-01-25 23:24:22 +0800

    20260125

--- 2.Binary/14.Missing Number.py
+++ 2.Binary/14.Missing Number.py
@@ -1,14 +1,45 @@
 # # Intuition
-# Given a positive integer n, returns the number of set bits in its binary representation (also known as the Hamming weight).
-# 計算正整數 n 的二進位表示中有多少個1，也就是 Hamming weight
+# 輸入一個陣列，理論上是從 0-n，輸出少了的人
 
 # # Approach
-# Brian Kernighan’s Algorithm (定義：一種計算 Hamming weight 的演算法)
-# REF: 
-# https://170530.medium.com/%E8%A8%88%E7%AE%97-1-%E5%80%8B-word-%E8%A3%A1%E6%9C%89%E5%B9%BE%E5%80%8B-on-%E7%9A%84-bit-00bad633f40d
+# 
 
 # # Complexity
 # - Time complexity:O(log n)
 # - Space complexity:O(log n)
 
 # language: Python
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # [法1] 總和公式 n*(n+1)//2 扣掉現在陣列加總
+        n = len(nums)
+        return n*(n+1)//2 - sum(nums)
+
+        # [法2] 利用 XOR 的特性：相同數字 XOR 會抵消
+        # [補充] XOR 的特性
+            # 相同數字 XOR 會抵消，例如：a ^ a = 0
+            # 任何數字和 0 XOR 還是自己，例如：a ^ 0 = a
+            # XOR 運算具交換律和結合律，順序不影響結果：a ^ b ^ c = c ^ a ^ b
+        # n = len(nums)
+        # xor_all = 0
+        # for i in range(n+1):
+        #     xor_all ^= i
+        # for num in nums:
+        #     xor_all ^= num
+        # return xor_all
+
+        # [法3] 排序法
+        # 如果少掉的是最後一個字元 要另外判斷
+        # if max(nums) != len(nums):
+        #     return len(nums)        
+        # nums.sort() # 將輸入做排序
+        # # only 少掉非最後一個字元，才跑回圈篩選
+        # for i, val in enumerate(nums):
+        #     if i != val:
+        #         return i
+
+        
--- 2.Binary/15.Reverse Bits.py
+++ 2.Binary/15.Reverse Bits.py
@@ -1,11 +1,8 @@
 # # Intuition
-# Given a positive integer n, returns the number of set bits in its binary representation (also known as the Hamming weight).
-# 計算正整數 n 的二進位表示中有多少個1，也就是 Hamming weight
+# 輸入一個陣列，理論上是從 0-n，輸出少了的人
 
 # # Approach
-# Brian Kernighan’s Algorithm (定義：一種計算 Hamming weight 的演算法)
-# REF: 
-# https://170530.medium.com/%E8%A8%88%E7%AE%97-1-%E5%80%8B-word-%E8%A3%A1%E6%9C%89%E5%B9%BE%E5%80%8B-on-%E7%9A%84-bit-00bad633f40d
+# 
 
 # # Complexity
 # - Time complexity:O(log n)
