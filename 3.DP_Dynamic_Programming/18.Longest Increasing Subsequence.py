fcdecaefdf972b20a3fce14f48868a42d0817b6d
Ruei-Syuan <syuan111251@gmail.com>
2026-01-26 23:59:52 +0800

    20260126

--- 3.DP_Dynamic_Programming/18.Longest Increasing Subsequence.py
+++ 3.DP_Dynamic_Programming/18.Longest Increasing Subsequence.py
@@ -1,14 +1,29 @@
 # # Intuition
-# Given a positive integer n, returns the number of set bits in its binary representation (also known as the Hamming weight).
-# 計算正整數 n 的二進位表示中有多少個1，也就是 Hamming weight
+# Given an integer array nums, return the length of the longest strictly increasing subsequence.
+# 輸入一個陣列，輸出此陣列中可以可遞增的子字串最長是多少
 
 # # Approach
-# Brian Kernighan’s Algorithm (定義：一種計算 Hamming weight 的演算法)
-# REF: 
-# https://170530.medium.com/%E8%A8%88%E7%AE%97-1-%E5%80%8B-word-%E8%A3%A1%E6%9C%89%E5%B9%BE%E5%80%8B-on-%E7%9A%84-bit-00bad633f40d
+# DP
+# REF:
+# https://medium.com/%E6%8A%80%E8%A1%93%E7%AD%86%E8%A8%98/leetcode-%E8%A7%A3%E9%A1%8C%E7%B4%80%E9%8C%84-300-longest-increasing-subsequence-f160358db4d1
 
 # # Complexity
-# - Time complexity:O(log n)
-# - Space complexity:O(log n)
+# - Time complexity:O(n^2)
+# - Space complexity:O(n)
 
 # language: Python
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        dp = [1] * n # 初始化 dp 陣列，每個位置的值為 1
+        
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i]:  # 檢查是否可以形成遞增子序列 
+                    dp[i] = max(dp[i], dp[j] + 1) # // 更新 dp[i] 值
+        
+        return max(dp) # 返回 dp 陣列中的最大值
--- 3.DP_Dynamic_Programming/19.Longest Common Subsequence.py
+++ 3.DP_Dynamic_Programming/19.Longest Common Subsequence.py
@@ -5,7 +5,6 @@
 # # Approach
 # Brian Kernighan’s Algorithm (定義：一種計算 Hamming weight 的演算法)
 # REF: 
-# https://170530.medium.com/%E8%A8%88%E7%AE%97-1-%E5%80%8B-word-%E8%A3%A1%E6%9C%89%E5%B9%BE%E5%80%8B-on-%E7%9A%84-bit-00bad633f40d
 
 # # Complexity
 # - Time complexity:O(log n)
