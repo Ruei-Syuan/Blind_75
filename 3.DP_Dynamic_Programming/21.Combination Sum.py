5a00bdd4dfbe531aa19c3819b17e30be03904bc7
Ruei-Syuan <syuan111251@gmail.com>
2026-03-02 00:17:43 +0800

    20260302

--- 3.DP_Dynamic_Programming/21.Combination Sum.py
+++ 3.DP_Dynamic_Programming/21.Combination Sum.py
@@ -1,14 +1,39 @@
 # # Intuition
-# Given a positive integer n, returns the number of set bits in its binary representation (also known as the Hamming weight).
-# 計算正整數 n 的二進位表示中有多少個1，也就是 Hamming weight
+# 輸入數列 & 加總目標, 輸出方法數
 
 # # Approach
-# Brian Kernighan’s Algorithm (定義：一種計算 Hamming weight 的演算法)
-# REF: 
-# https://170530.medium.com/%E8%A8%88%E7%AE%97-1-%E5%80%8B-word-%E8%A3%A1%E6%9C%89%E5%B9%BE%E5%80%8B-on-%E7%9A%84-bit-00bad633f40d
-
+# DP- 回溯法（Backtracking）
+# 思路: 利用 DP，每次答案等於上一次解法+1的邏輯
+        
 # # Complexity
 # - Time complexity:O(log n)
 # - Space complexity:O(log n)
 
 # language: Python
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # init
+        dp = [0]*(target+1)
+        dp[0] = 1 # 第一層一定只有 1 種方法
+
+        # 思路: 利用 DP，每次扣一的解法+1
+        # 計算組合
+        # for n in nums:
+        #     for i in range(n, target + 1):
+        #         dp[i] += dp[i-n]
+
+        # 排序 
+        for t in range(1, target + 1):
+            for num in nums:
+                if t - num >= 0:
+                    dp[t] += dp[t - num]
+
+        return dp[target]
+                
+ss = Solution()
+print(ss.combinationSum4([1,2,3], 4))
