bc9b33cdca0c9a42fb52b63683941f6a27f03782
Ruei-Syuan <syuan111251@gmail.com>
2026-03-05 00:21:59 +0800

    20260305

--- 3.DP_Dynamic_Programming/24.Decode Ways.py
+++ 3.DP_Dynamic_Programming/24.Decode Ways.py
@@ -1,14 +1,37 @@
 # # Intuition
-# Given a positive integer n, returns the number of set bits in its binary representation (also known as the Hamming weight).
-# 計算正整數 n 的二進位表示中有多少個1，也就是 Hamming weight
+# 編碼 1~26, 請問輸入字串可以有幾種解碼方式?
 
 # # Approach
-# Brian Kernighan’s Algorithm (定義：一種計算 Hamming weight 的演算法)
-# REF: 
-# https://170530.medium.com/%E8%A8%88%E7%AE%97-1-%E5%80%8B-word-%E8%A3%A1%E6%9C%89%E5%B9%BE%E5%80%8B-on-%E7%9A%84-bit-00bad633f40d
+# DP，每一步的答案 = 「能單獨解碼，加上前一步的解法」 + 「能兩個字一起解碼，加上前兩步解法」
 
 # # Complexity
-# - Time complexity:O(log n)
-# - Space complexity:O(log n)
+# - Time complexity:O(n)
+# - Space complexity:O(n)
 
 # language: Python
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s or s[0] == "0":
+            return 0
+
+        n = len(s)
+        dp = [0] * (n + 1)
+        # dp[len(s)] = 0*len(s)
+        dp[0] = 1  # 空字串
+        dp[1] = 1  # 第一個字元不是 0
+
+        for i in range(2, n + 1):
+            # 單字元能解碼
+            # - 我們在算 dp[i] 的時候，對應的字元是 s[i-1]
+            # - dp[1] → 前 1 個字元 "2" → 對應 s[0]
+            if s[i-1] != "0":
+                dp[i] += dp[i-1]
+            # 雙字元能解碼 (10~26)
+            if "10" <= s[i-2:i] <= "26":
+                dp[i] += dp[i-2]
+        
+        return dp[-1]
