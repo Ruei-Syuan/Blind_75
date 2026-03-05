0cbe154b6e37d7d2a4e3f9cc05e471cc9fca0df5
Ruei-Syuan <syuan111251@gmail.com>
2026-03-06 00:31:20 +0800

    20260306

--- 3.DP_Dynamic_Programming/25.Unique Paths.py
+++ 3.DP_Dynamic_Programming/25.Unique Paths.py
@@ -1,14 +1,50 @@
 # # Intuition
-# Given a positive integer n, returns the number of set bits in its binary representation (also known as the Hamming weight).
-# 計算正整數 n 的二進位表示中有多少個1，也就是 Hamming weight
+# 輸入 m*n 的棋盤大小，輸出從此棋盤左上角到右下角的 總步數
 
 # # Approach
-# Brian Kernighan’s Algorithm (定義：一種計算 Hamming weight 的演算法)
-# REF: 
-# https://170530.medium.com/%E8%A8%88%E7%AE%97-1-%E5%80%8B-word-%E8%A3%A1%E6%9C%89%E5%B9%BE%E5%80%8B-on-%E7%9A%84-bit-00bad633f40d
+# [法一] 排列組合 (此題屬於 組合 DDRR, 排列會把 D 分成 D1 D2)
+# [法二] DP，此步驟 = 左邊 + 上面
+# - DP 是「一步一步累積」的邏輯，像在算路徑表格。
+# - 排列組合 是「直接公式」的邏輯，像在算步數的分配。
+# - 如果網格很單純 → 用組合公式最快。
+# - 如果網格有障礙物或特殊規則 → 必須用 DP
 
 # # Complexity
-# - Time complexity:O(log n)
-# - Space complexity:O(log n)
+# - Time complexity:O(M+N)
+# - Space complexity:O(1)
 
 # language: Python
+class Solution(object):
+    def factorial(self, n):
+        # 計算 n! 階乘
+        result = 1
+        for i in range(1, n+1):
+            result *= i
+        return result
+
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        # [法一] DP法
+        # 建立 m x n 的表格，初始值都設為 1
+        # dp = [[1] * n for _ in range(m)]
+        # # 從第二行、第二列開始填表
+        # for i in range(1, m):
+        #     for j in range(1, n):
+        #         # 每個格子的路徑數 = 上方 + 左方
+        #         dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        # return dp[m-1][n-1]
+        
+        # [法二] 組合法
+        totalSteps = (m - 1) + (n - 1)   # 總步數
+        downSteps  = (m - 1)             # 向下的步數
+            
+        # 組合公式 C(totalSteps, downSteps) = totalSteps! / (downSteps! * (totalSteps - downSteps)!)
+        return factorial(totalSteps) // (factorial(downSteps) * factorial(totalSteps - downSteps))
+
+# print(uniquePathsDP(3, 7))  # 輸出: 28
+        
